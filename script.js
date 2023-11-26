d559b489f4d04ffbc1f96552c5d8a508e8cf2391
TwilightLogic <twilightlogic@gmail.com>
2023-11-27 01:15:07 +0800

    update CV

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const navbarItems = document.querySelectorAll(".item");
 
 for (let i = 0; i < navbarItems.length; i++) {
-  navbarItems[i].addEventListener("click", function () {
-    for (let i = 0; i < navbarItems.length; i++) {
-      navbarItems[i].classList.remove("active");
-    }
-    this.classList.add("active");
-  });
+    navbarItems[i].addEventListener("click", function () {
+        for (let i = 0; i < navbarItems.length; i++) {
+            navbarItems[i].classList.remove("active");
+        }
+        this.classList.add("active");
+    });
 }
 
 const typed = new Typed(".multiple-text", {
-  strings: ["Frontend Developer", "BiliBili Uploader", "TikTok User"],
-  typeSpeed: 100,
-  backSpeed: 100,
-  backDelay: 1000,
-  loop: true,
+    strings: ["Frontend Developer", "Web3 Buidler", "English Enthusiast"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
 });
